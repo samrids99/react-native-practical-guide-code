f9fd3437cfeb3714a784904a6d2bb55a2956182e
Sam Ridsdale <samridsdale108@gmail.com>
2026-09-01 20:28:08 +0100

    extracting the auth token

--- code/01-starting-code/screens/LoginScreen.js
+++ code/01-starting-code/screens/LoginScreen.js
@@ -1,16 +1,20 @@
 import AuthContent from "../components/Auth/AuthContent";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { login } from "../util/auth";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
+  const authCtx = useContext(AuthContext);
+
   async function loginHandler({ email, password }) {
     setIsAuthenticating(true);
     try {
-      await login(email, password);
+      const token = await login(email, password);
+      authCtx.authenticate(token);
     } catch (error) {
       Alert.alert("Login Failed", "Please check your credentials");
     }
