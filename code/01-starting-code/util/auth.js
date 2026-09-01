aca64a0ee1a66610379e139a6ffb5495a6e62099
Sam Ridsdale <samridsdale108@gmail.com>
2026-09-01 18:47:20 +0100

    creating new users

--- code/01-starting-code/util/auth.js
+++ code/01-starting-code/util/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = "AIzaSyBOOk5xN9iy2i3txOAn_20UZM3kCsGjpQQ";
 
-async function createUser(email, password) {
+export async function createUser(email, password) {
   const response = await axios.post(
     "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
     {
