36b5cd86610689170a0834831b03ab5a2fb5fe4f
Sam Ridsdale <samridsdale108@gmail.com>
2026-09-01 18:39:52 +0100

    Sending Authentication requests to backend

---
+++ code/01-starting-code/util/auth.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_KEY = "AIzaSyBOOk5xN9iy2i3txOAn_20UZM3kCsGjpQQ";
+
+async function createUser(email, password) {
+  const response = await axios.post(
+    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=" + API_KEY,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    },
+  );
+}
